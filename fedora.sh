e589943aee4d51fddc8f8192ba029b72c6a87296
Zvarun Volodymyr <86479624+lolpie244@users.noreply.github.com>
2022-10-16 23:02:03 +0300

    Add files via upload

---
+++ fedora.sh
@@ -0,0 +1,94 @@
+if [ $EUID != 0 ]; then
+    sudo "$0" "$@"
+    exit $?
+fi
+
+init()
+{
+    sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+    sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+        sudo echo "fastestmirror=true
+deltarpm=true
+max_parallel_downloads=10" >> /etc/dnf/dnf.conf
+
+    sudo dnf autoremove
+    sudo dnf clean all
+    sudo dnf upgrade --refresh
+
+    printf "enabled rpm_fusion, optitimised dnf\n\n"
+
+    printf "set su password:"
+    sudo passwd root
+}
+
+package_managers()
+{
+    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+    sudo dnf install snapd
+    sudo systemctl enable --now snapd.socket
+    sudo ln -s /var/lib/snapd/snap /snap 
+    
+    printf "enabled flathub, installed snap \n\n"
+}
+
+codecs() 
+{
+    sudo dnf install vlc nomacs
+    sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
+    sudo dnf install lame\* --exclude=lame-devel
+    sudo dnf group upgrade --with-optional Multimedia
+    
+    printf "codecs installed\n\n"
+}
+
+
+battery() 
+{
+    sudo dnf install tlp pip
+    sudo snap install auto-cpufreq
+    git clone https://github.com/geminis3/envycontrol.git
+
+    cd envycontrol
+    sudo pip install .
+    cd ..
+
+    printf "installed: tlp, auto-cpufreq, envycontrol\n\n"
+}
+
+software() 
+{
+    sudo dnf copr enable principis/NoiseTorch
+
+    sudo dnf install timeshift noisetorch gnome-shell-extension-pop-shell gnome-tweaks gcc dotnet-sdk-6.0 dotnet-runtime-6.0 mono-devel aspnetcore-runtime-6.0 htop
+    sudo flatpak install qbittorrent zoom teams_for_linux com.obsproject.Studio onlyoffice xournalpp org.gnome.NetworkDisplays krita
+}
+
+neovim() 
+{
+    sudo dnf install neovim
+}
+postgres() 
+{
+    sudo dnf install postgresql-server postgresql-contrib
+    sudo systemctl enable postgresql
+    sudo postgresql-setup --initdb --unit postgresql
+    sudo systemctl start postgresql
+    # sudo -u postgres psql
+}
+
+
+
+
+case $1 in
+    "init") init;;
+    "package_managers") package_managers;;
+    "codecs") codecs;;
+    "battery") battery;;
+    "software") software;;
+    "neovim") neovim;;
+    "postgres") postgres;;
+    *) 
+	printf "invalid argument\n"
+	exit 1;;
+esac
---
+++ setup.sh
@@ -0,0 +1,135 @@
+#if [ $EUID != 0 ]; then
+#    sudo -s "$0" "$@"
+#    exit $?
+#fi
+
+ask_to_run()
+{
+    while true; do
+	printf "\n=======================================================\n"
+	read -p "$1?(y,n): " yn
+	case $yn in
+	    [Yy]* ) 
+		return 0;;
+	    [Nn]* ) 
+		return 1;;
+	    * ) echo "Please answer yes or no.";;
+	esac
+    done
+}
+
+distr_list_message="0: arch; 1: fedora"
+distr_script_list=([0]="arch.sh" [1]="fedora.sh")
+
+
+while true; do
+    read -p "Write number of your distro: (${distr_list_message[*]}): " dist
+    if [ "${distr_script_list[$dist]+abc}" ]; then
+	    distr_script=${distr_script_list[$dist]}
+	    break
+    fi
+    echo "Invalid number, try again"
+
+done
+
+chmod +x $distr_script
+
+if ask_to_run "Do init settings"; then
+    sudo ./$distr_script init
+
+fi
+# remap keyboard
+if ask_to_run "Swap insert and z keys"; then
+
+    sudo sed -i 's/118/TO_CHANGE/g' /usr/share/X11/xkb/keycodes/evdev
+    sudo sed -i 's/52/118/g' /usr/share/X11/xkb/keycodes/evdev
+    sudo sed -i 's/TO_CHANGE/118/g' /usr/share/X11/xkb/keycodes/evdev
+fi
+
+if ask_to_run "Install additional package managers"; then
+    sudo ./$distr_script package_managers
+fi
+
+
+if ask_to_run "Install codecs"; then
+    sudo ./$distr_script codecs
+fi
+
+
+if ask_to_run "Optimize battery life"; then
+    sudo ./$distr_script battery
+    
+    yes | cp -rf tlp.conf /etc
+    sudo systemctl enable --now tlp.service
+    sudo snap run auto-cpufreq --install
+    sudo tlp start
+    sudo systemctl mask power-profiles-daemon.service
+    sudo systemctl mask systemd-rfkill.service
+    sudo systemctl mask systemd-rfkill.socket
+
+    
+    cp -r scripts "$HOME"
+    cd
+    chmod +x scripts/config
+    cd -
+fi
+
+
+if ask_to_run "Install neovim"; then
+    sudo ./$distr_script neovim
+    cp -r nvim/ ${HOME}/.config
+    git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+    nvim -c PackerSync
+fi
+
+if ask_to_run "Install postgresql"; then
+    sudo ./$distr_script postgres
+    
+    sudo systemctl enable --now postgresql
+
+    if ask_to_run "Install pgadmin"; then
+	sudo mkdir /var/lib/pgadmin
+	sudo mkdir /var/log/pgadmin 
+	
+	sudo chown $USER /var/lib/pgadmin
+	sudo chown $USER /var/log/pgadmin
+	
+	current=`pwd`
+
+	cd
+	mkdir ${HOME}/scripts
+	mkdir ${HOME}/scripts/pgadmin
+	cd scripts/pgadmin
+	python3 -m venv venv
+	venv/bin/pip install pgadmin4 gevent
+	echo "source venv/bin/activate" > pgadmin
+	echo "pgadmin4" >> pgadmin
+	chmod +x pgadmin 
+
+	cd $current
+    fi
+fi
+if ask_to_run "Install other software"; then
+    sudo ./$distr_script software
+fi
+
+if ask_to_run "Move home folders to hdd"; then
+
+    read -p "Path to hdd: " path
+
+    folders_list=(Documents Pictures Videos Downloads Music)
+    
+    cd
+    echo $HOME
+    for folder in ${folders_list[@]}
+    do
+	echo `pwd`/$folder
+	sudo rm -R $folder
+	if ! [ -d ${path}/$folder ]; then
+	    mkdir ${path}/$folder
+	fi
+	ln -s ${path}/$folder $folder
+    done
+    cd -
+fi
